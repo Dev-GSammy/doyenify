bc12bdfe843671c2d713cb13c4feef66cec85512
Olumide Oluyide <festusoluyide@gmail.com>
2023-02-24 00:53:29 +0200

    Social links added

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -27,25 +27,25 @@ const Footer: React.FC = () => {
 
       <Col sm={3} className="text-center "><h5 className="text-black">Contact</h5>
       <p><span></span></p>
-      <div className="text-black">info@doyenify.tech</div>
+      <a href="mailto:info@doyenify.tech" className="text-black">info@doyenify.tech</a>
       <p><span></span></p>
-      <div className="text-black">08034657796</div>
+      <a href="tel:+2348135361806" className="text-black">08034657796</a>
       <p><span></span></p>
-      <div className="text-black">78697840600</div>
+      <a href="tel:+2348135361806" className="text-black">78697840600</a>
       </Col>
 
      
       <Col sm={3} className="text-center"><h5 className="text-black"> Social Media </h5>
       <p><span></span></p>
-      <div className="text-black"><Linkedin/> </div>
+      <a href="https://www.linkedin.com/company/doyenify/" className="text-black"><Linkedin/> </a>
       <p><span></span></p>
-      <div className="text-black"><Instagram/> </div>
+      <a href="https://www.instagram.com/doyenify/?igshid=ZDdkNTZiNTM%3D" className="text-black"><Instagram/> </a>
       <p><span></span></p>
-      <div className="text-black"><Facebook/> </div>
+      <a href="#" className="text-black"><Facebook/> </a>
       <p><span></span></p>
-      <div className="text-black"><Twitter/></div>
+      <a href="https://twitter.com/DOYENIFY?t=iIP1-2SvQTGkMvpng9Fddw&s=09" className="text-black"><Twitter/></a>
       <p><span></span></p>
-      <div className="text-black"><Youtube/> </div>
+      <a href="#" className="text-black"><Youtube/> </a>
       </Col>
     
       <Col sm={3} className="text-center "><h5 className="text-black"> Address </h5>
