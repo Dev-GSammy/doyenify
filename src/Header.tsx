d857bb94df814a14dba67557db2bed08fa01fe56
Olumide Oluyide <festusoluyide@gmail.com>
2023-02-25 17:26:45 +0200

    Merge branch 'main' of https://github.com/doyenify/doyenify into HEAD
    # Conflicts:
    #	src/Layout.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -17,11 +17,11 @@ const Header: React.FC = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="justify-content-end flex-grow-1 pe-3">
           <Nav.Link href="/" className="ms-5 text-black">Home</Nav.Link>
-            <Nav.Link href="#features" className="ms-5 text-black">Our Work</Nav.Link>
+            <Nav.Link href="/Ourwork" className="ms-5 text-black">Our Work</Nav.Link>
             <Nav.Link href="/About_us" className="ms-5 text-black" >About Us</Nav.Link>
             <Nav.Link href="/Services" className="ms-5 text-black">What We Do</Nav.Link>
             <Nav.Link href="/Our_Team" className="ms-5 text-black">Our Team</Nav.Link>
-            <Button href="#" className="ms-5" variant="success">Contact Us</Button>
+            <Button href="/Contact" className="ms-5" variant="success">Contact Us</Button>
           </Nav>
           </Navbar.Collapse>
         </Container>
