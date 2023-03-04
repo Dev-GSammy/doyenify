10a56916752bfc30dbf9ad676aa7edf68b69bcfd
Toyib Ahmed <ahmedtoyib1@gmail.com>
2023-03-04 17:56:01 +0200

    Update what-we-do page

--- src/pages/WhatWeDo.tsx
+++ src/pages/WhatWeDo.tsx
@@ -1,108 +1,71 @@
 import React from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
+const sections = [
+  {
+    url: "./img/pharm.jpg",
+    title: "Pharm Solution",
+    text: "Our pharm solution affords patients easy access to prescription medications from our online retail pharmacy",
+    buttonText: "Read More",
+  },
+  {
+    url: "./img/payrool.jpg",
+    title: "Payroll Solution",
+    text: "The Doyenify payroll solution is a simple-to-use software that allows organisations to integrate performance measurement.",
+    buttonText: "Read More",
+  },
+  {
+    url: "./img/ecommerce.jpg",
+    title: "E-Commerce",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    buttonText: "Read More",
+  },
+];
+
 const WhatWeDo = () => (
-  <div>
-    <main role="main" className="container">
+  <>
+    <main role="main" className="container-fluid px-0">
+      <div className="text-center">
+        <Card className="bg-dark text-white" style={{ borderRadius: 0 }}>
+          <Card.Img src="./img/about.png" alt="about" />
+          <Card.ImgOverlay className="d-flex flex-column align-items-center justify-content-center">
+            <Card.Title className="text-center" style={{ color: "#00FF38" }}>
+              <h2>What We Do</h2>
+            </Card.Title>
+            <Card.Text>
+              We would be responsible for coding and building the platform that
+              <br />
+              allows users to access educational content. This would likely
+              <br />
+              involve using programming languages such as Java, Python, or
+              JavaScript to develop the backend and frontend of the platform.
+              <br />
+              We would also need to maintain and update the platform over time
+              to ensure it remains secure, efficient, and up-to-date.
+            </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+      </div>
       <div className="mt-5">
-        <Row>
-          <Col sm={6}>
-            <img
-              src="./img/Rectangle72.png"
-              alt="Rectangle72"
-              width="500"
-              height="300"
-              className="rounded"
-            />
-          </Col>
-          <Col>
-            <h2 className=" text-left text-success ">An Overview of </h2>
-            <h2 className=" text-left text-success "> what we do</h2>
-            <p>
-              <span></span>
-            </p>
-            <div>
-              {" "}
-              We are an IT outfit with experienced technical and business
-              professionals dedicated to developing technologically driven
-              solutions to help organisations scale their businesses.
-            </div>
-          </Col>
-        </Row>
-        <div className="my-3 p-3 rounded shadow-sm">
-          <h3 className="text-center text-success  mb-3">
-            Our Digital Products
-          </h3>
-          <div className="text-center">
-            We utilize the latest innovations, digital technologies and
-            methodologies,
-          </div>
-          <div className="text-center">
-            to create a valuable experience and improve your digital experience.
-          </div>
-          <p>
-            <span></span>
-          </p>
-          <Container>
-            <Row className="mb-5 text-center">
+        <Container>
+          <Row className="mb-5 text-center">
+            {sections.map(({ url, title, text, buttonText }) => (
               <Col>
                 <Card style={{ width: "18rem" }} className="mb-3">
-                  <img src="./img/pharm.jpg" alt="pharm" className="rounded" />
+                  <img src={url} alt={title} className="rounded" />
                   <Card.Body>
-                    <Card.Title>Pharm Solution</Card.Title>
-                    <Card.Text>
-                      <p>
-                        Our pharm solution affords patients easy access to
-                        prescription medications from our online retail pharmacy
-                      </p>
-                    </Card.Text>
-                    <Button variant="success">Read More</Button>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{text}</Card.Text>
+                    <Button variant="success">{buttonText}</Button>
                   </Card.Body>
                 </Card>
               </Col>
-              <Col>
-                <Card style={{ width: "18rem" }} className="mb-3">
-                  <img
-                    src="./img/payrool.jpg"
-                    alt="payrool"
-                    className="rounded"
-                  />
-                  <Card.Body>
-                    <Card.Title>Payroll Solution</Card.Title>
-                    <Card.Text>
-                      <p>
-                        The Doyenify payroll solution is a simple-to-use
-                        software that allows organisations to integrate
-                        performance measurement,
-                      </p>
-                    </Card.Text>
-                    <Button variant="success">Read More</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col>
-                <Card style={{ width: "18rem" }} className="mb-3">
-                  <img
-                    src="./img/ecommerce.jpg"
-                    alt="ecommerce"
-                    className="rounded"
-                  />
-                  <Card.Body>
-                    <Card.Title>E-Commerce</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="success">Read More</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+            ))}
+          </Row>
+        </Container>
       </div>
     </main>
-  </div>
+  </>
 );
 
 export default WhatWeDo;
