a7b80f12d8adf8b3a1ac73d6d9e07160a82ce213
TayeCaleb <calebmide@gmail.com>
2023-07-06 12:23:54 +0100

    another change

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,7 @@ import { Container, Spinner } from "react-bootstrap";
 import Header from "./Header";
 import Footer from "./Footer";
 import { LiveChatLoaderProvider } from 'react-live-chat-loader'
-import Home from "./pages/Homepage";
-import OurWork from "./pages/OurWork";
-import AboutUs from "./pages/AboutUs";
-import WhatWeDo from "./pages/WhatWeDo";
-import Contact from "./pages/Contact";
-import OurTeam from "./pages/OurTeam";
+
 
 const navigationData = [
   { label: "Home", path: "/" },
@@ -19,12 +14,12 @@ const navigationData = [
   { label: "Our Team", path: "/our_Team" },
 ];
 
-// const HomeView = React.lazy(() => import("./pages/Homepage"));
-// const OurWorkView = React.lazy(() => import("./pages/"));
-// const AboutUsView = React.lazy(() => import("./pages/AboutUs"));
-// const WhatWeDoView = React.lazy(() => import("./pages/WhatWeDo"));
-// const ContactView = React.lazy(() => import("./pages/Contact"));
-// const OurTeamView = React.lazy(() => import("./pages/OurTeam"));
+const HomeView = React.lazy(() => import("./pages/Homepage"));
+const OurWorkView = React.lazy(() => import("./pages/OurWork"));
+const AboutUsView = React.lazy(() => import("./pages/AboutUs"));
+const WhatWeDoView = React.lazy(() => import("./pages/WhatWeDo"));
+const ContactView = React.lazy(() => import("./pages/Contact"));
+const OurTeamView = React.lazy(() => import("./pages/OurTeam"));
 
 export const App = () => (
   <>
@@ -34,18 +29,12 @@ export const App = () => (
     <Container fluid className="px-0">
       <React.Suspense fallback={<Spinner animation={"border"} />}>
         <Routes>
-          {/* <Route path="/" element={<HomeView />} />
+          <Route path="/" element={<HomeView />} />
           <Route path="/our_work" element={<OurWorkView />} />
           <Route path="/about_us" element={<AboutUsView />} />
           <Route path="/services" element={<WhatWeDoView />} />
           <Route path="/contact" element={<ContactView />} />
-           <Route path="our_team" element={<OurTeamView />} /> */}
-           <Route path="/" element={<Home />} />
-          <Route path="/our_work" element={<OurWork />} />
-          <Route path="/about_us" element={<AboutUs />} />
-          <Route path="/services" element={<WhatWeDo />} />
-          <Route path="/contact" element={<Contact />} />
-           <Route path="our_team" element={<OurTeam />} />
+           <Route path="our_team" element={<OurTeamView />} />
         </Routes>
       </React.Suspense>
     </Container>
